4544626321ab59d86824603d0aa98e7022417b46
TKOFFICE\huxw <huxw@mail.jj.cn>
2019-12-05 17:49:46 +0800

    暂存代码

--- docs/test.js
+++ docs/test.js
@@ -1,16 +1,16 @@
-import getManager, { DanmuManager } from "./lib/index";
 
-const videoEl = document.querySelector("video");
-let isPlayed = false;
-videoEl.oncanplay = function() {
+
+var videoEl = document.querySelector("video");
+var isPlayed = false;
+videoEl.oncanplay = function () {
     if (!isPlayed) {
         isPlayed = true;
     }
 };
-const containerEl = document.getElementById("container");
-var manager: DanmuManager | null = null;
-manager = getManager();
-(window as any).manager = manager;
+var containerEl = document.getElementById("container");
+var manager = null;
+manager = window.DanmuManager();
+window.manager = manager;
 manager.init(containerEl, [
     {
         duration: 10000,
@@ -26,9 +26,8 @@ manager.init(containerEl, [
     }
 ]);
 manager.start();
-let ticket = 0;
-
-const pools = [
+var ticket = 0;
+var pools = [
     { content: "完结撒花完结撒花完结撒花", style: "color:Red", duration: 10000 },
     { content: "25.5啥的也算一级", style: "color:blue", duration: 8000 },
     // {
@@ -65,66 +64,53 @@ const pools = [
         duration: 6000
     }
 ];
-
-function getRandomIndex(len: number) {
+function getRandomIndex(len) {
     return ~~(Math.random() * len);
 }
-
-function batchGet(count: number) {
-    const r = [];
-    const len = pools.length;
-    for (let i = 0; i < count; i++) {
+function batchGet(count) {
+    var r = [];
+    var len = pools.length;
+    for (var i = 0; i < count; i++) {
         r.push(pools[getRandomIndex(len)]);
     }
     return r;
 }
-
-const txtIntervalEl = document.getElementById("txtInterval") as HTMLInputElement;
-const txtBatchCount = document.getElementById("txtBatchCount") as HTMLInputElement;
+var txtIntervalEl = document.getElementById("txtInterval");
+var txtBatchCount = document.getElementById("txtBatchCount");
 function startBatch() {
-    const batchCount = +txtBatchCount.value;
+    var batchCount = +txtBatchCount.value;
     manager.sendDanmu(batchGet(batchCount));
-    ticket = window.setInterval(function() {
+    ticket = window.setInterval(function () {
         manager.sendDanmu(batchGet(batchCount));
     }, +txtIntervalEl.value);
 }
-
-let isBigTest = false;
-const txtDanmuEl: HTMLInputElement = document.getElementById("txtDanmu") as HTMLInputElement;
-document.getElementById("btnSend").addEventListener(
-    "click",
-    ev => {
-        manager.sendDanmu(txtDanmuEl.value);
-        ev.stopPropagation();
-    },
-    false
-);
-
-document.getElementById("btnPause").addEventListener("click", () => {
+var isBigTest = false;
+var txtDanmuEl = document.getElementById("txtDanmu");
+document.getElementById("btnSend").addEventListener("click", function (ev) {
+    manager.sendDanmu(txtDanmuEl.value);
+    ev.stopPropagation();
+}, false);
+document.getElementById("btnPause").addEventListener("click", function () {
     if (isBigTest) {
         clearInterval(ticket);
     }
     manager.pause();
 });
-
-document.getElementById("btnContiue").addEventListener("click", () => {
+document.getElementById("btnContiue").addEventListener("click", function () {
     if (isBigTest) {
         startBatch();
     }
-    manager.continue();
+    manager["continue"]();
 });
-
-document.getElementById("btnStart").addEventListener("click", () => {
+document.getElementById("btnStart").addEventListener("click", function () {
     manager.start();
 });
-
-document.getElementById("btnStop").addEventListener("click", () => {
+document.getElementById("btnStop").addEventListener("click", function () {
     manager.stop();
     clearInterval(ticket);
     isBigTest = false;
 });
-
-document.getElementById("btnBigTest").addEventListener("click", () => {
+document.getElementById("btnBigTest").addEventListener("click", function () {
     if (isBigTest) {
         return;
     }
@@ -133,9 +119,9 @@ document.getElementById("btnBigTest").addEventListener("click", () => {
     startBatch();
     isBigTest = true;
 });
-(function() {
+(function () {
     var script = document.createElement("script");
-    script.onload = function() {
+    script.onload = function () {
         var stats = new window.Stats();
         document.body.appendChild(stats.dom);
         requestAnimationFrame(function loop() {
@@ -146,42 +132,36 @@ document.getElementById("btnBigTest").addEventListener("click", () => {
     script.src = "//mrdoob.github.io/stats.js/build/stats.min.js";
     document.head.appendChild(script);
 })();
-
-const lbTotal = document.getElementById("lbTotal");
-const lbHide = document.getElementById("lbHide");
-const lbInView = document.getElementById("lbInView");
-
-const rect = containerEl.getBoundingClientRect();
-const { left, width } = rect;
-const right = left + width;
-setInterval(function() {
-    (window as any).requestIdleCallback(() => {
-        let startTime = performance.now();
-        const allItems = Array.from(containerEl.querySelectorAll(".danmu-item"));
-
-        const accCount = document.querySelectorAll(".danmu-item-acc").length;
-        const len = allItems.length;
-        const inHideLen = allItems.filter(item => item.classList.contains("hide")).length;
-        const inViewLen = allItems.filter(function(item) {
-            const rect = item.getBoundingClientRect();
-            const b = !item.classList.contains("hide") && rect.left + rect.width >= left;
+var lbTotal = document.getElementById("lbTotal");
+var lbHide = document.getElementById("lbHide");
+var lbInView = document.getElementById("lbInView");
+var rect = containerEl.getBoundingClientRect();
+var left = rect.left, width = rect.width;
+var right = left + width;
+setInterval(function () {
+    window.requestIdleCallback(function () {
+        var startTime = performance.now();
+        var allItems = Array.from(containerEl.querySelectorAll(".danmu-item"));
+        var accCount = document.querySelectorAll(".danmu-item-acc").length;
+        var len = allItems.length;
+        var inHideLen = allItems.filter(function (item) { return item.classList.contains("hide"); }).length;
+        var inViewLen = allItems.filter(function (item) {
+            var rect = item.getBoundingClientRect();
+            var b = !item.classList.contains("hide") && rect.left + rect.width >= left;
             return b;
         }).length;
-
         lbTotal.innerText = len + "-" + accCount;
         lbHide.innerHTML = inHideLen + "";
         lbInView.innerHTML = inViewLen + " (本次统计耗时" + (performance.now() - startTime).toFixed(2) + "ms)";
     });
 }, 5000);
-
-document.addEventListener("visibilitychange", function() {
+document.addEventListener("visibilitychange", function () {
     // 用户离开了当前页面
     if (document.visibilityState === "hidden") {
         manager.stop();
         console.log("stop....");
         // console.log(document.getElementById("frames_frame1").getBoundingClientRect())
     }
-
     // 用户打开或回到页面
     if (document.visibilityState === "visible") {
         manager.start();
@@ -189,8 +169,7 @@ document.addEventListener("visibilitychange", function() {
         // console.log(document.getElementById("frames_frame1").getBoundingClientRect())
     }
 });
-
-document.addEventListener("fullscreenchange", function() {
+document.addEventListener("fullscreenchange", function () {
     console.log("fullscreenchange");
     if (!document.fullscreenElement) {
         quitFull();
@@ -198,27 +177,23 @@ document.addEventListener("fullscreenchange", function() {
     }
     goFull();
 });
-
-document.getElementById("btnFull").addEventListener("click", ev => {
+document.getElementById("btnFull").addEventListener("click", function (ev) {
     ev.stopPropagation();
-    document.body.requestFullscreen().then(() => {
-        setTimeout(() => {
+    document.body.requestFullscreen().then(function () {
+        setTimeout(function () {
             goFull();
         }, 0);
     });
 });
-
 function goFull() {
     containerEl.classList.add("fullScreen");
     videoEl.classList.add("fullScreen");
     videoEl.setAttribute("webkit-playsinline", "");
     videoEl.setAttribute("playsinline", "");
-
     manager.resize({
         duration: 10000
     });
 }
-
 function quitFull() {
     containerEl.style.width = "1349px";
     containerEl.style.height = "758px";
