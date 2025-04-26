212bb2dd31dab012088b9cbff65acb6668193706
huxw <huxw@mail.jj.cn>
2025-04-26 16:59:57 +0800

    git和图片

--- docs/test.js
+++ docs/test.js
@@ -1,5 +1,3 @@
-
-
 var videoEl = document.querySelector("video");
 var isPlayed = false;
 videoEl.oncanplay = function () {
@@ -14,55 +12,59 @@ window.manager = manager;
 manager.init(containerEl, [
     {
         duration: 10000,
-        useMeasure: true
+        useMeasure: true,
+    },
+    {
+        duration: 8000,
+        useMeasure: true,
     },
     {
         duration: 6000,
-        useMeasure: true
+        useMeasure: true,
     },
-    // {
-    //     duration: 6000,
-    //     useMeasure: true
-    // }
 ]);
 manager.start();
 var ticket = 0;
 var pools = [
-    { content: "完结撒花完结撒花完结撒花", style: "color:Red", duration: 10000 },
+    {
+        content: "完结撒花完结撒花完结撒花",
+        style: "color:Red",
+        duration: 10000,
+    },
     { content: "25.5啥的也算一级", style: "color:#FFFFFF", duration: 8000 },
-    // {
-    //     forceDetect: true,
-    //     duration:5000,
-    //     content:
-    //         "<img src='//static.hdslb.com/images/member/noface.gif' style='height:20px;vertical-align: middle;'>留下jo印留下jo印留下jo印"
-    // }
-    // {  duration:5000, render: "高价回收天堂之眼，不要问我为什么" },
-    // {
-    //     render: ({ left, top }) => {
-    //         const el = document.createElement("span");
-    //         el.innerHTML = "麦姐在学院除了老大老二基本就是最厉害的了 from span";
-    //         el.style.left = left + "px";
-    //         el.style.top = top + "px";
-    //         // el.style.zIndex = "999";
-    //         // el.style.backgroundColor = "#666";
-    //         return el;
-    //     }
-    // },
-    // // {
-    // //     content:
-    // //         "<img src='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565416527128&di=86e45c16db43679153f959bbe08a3f51&imgtype=0&src=http%3A%2F%2Fimg.xinxic.com%2Fimg%2F35167cff7ee71983.jpg' style='height:60px'/>",
-    // //     forceDetect: true,
-    // //     style: "z-index:99;"
-    // // },
-    // "好假炮姐当年1v3有一个5的和两个4的都打得过",
-    // "这个女的好帅啊，一拳一个机器人的那个",
-    // "哇喔哇喔哇喔哇喔好燃啊！！！",
-    // "黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴",
+    {
+        forceDetect: true,
+        duration: 6000,
+        content:
+            "<img src='https://img.soogif.com/VO7KJY9mZv3FByd2Igf7K9fGlRlmursf.gif' style='height:30px;vertical-align: middle;'>留下jo印留下jo印留下jo印",
+    },
+    { duration: 6000, render: "高价回收天堂之眼，不要问我为什么" },
+    {
+        render: ({ left, top }) => {
+            const el = document.createElement("span");
+            el.innerHTML = "麦姐在学院除了老大老二基本就是最厉害的了 from span";
+            el.style.left = left + "px";
+            el.style.top = top + "px";
+            // el.style.zIndex = "999";
+            // el.style.backgroundColor = "#666";
+            return el;
+        },
+    },
+    {
+        content:
+            "<img src='https://p6-passport.byteacctimg.com/img/user-avatar/f1a9f122e925aeef5e4534ff7f706729~90x90.awebp' style='height:20px'/> 头像哦",
+        forceDetect: true,
+        style: "z-index:99;",
+    },
+    "好假炮姐当年1v3有一个5的和两个4的都打得过",
+    "这个女的好帅啊，一拳一个机器人的那个",
+    "哇喔哇喔哇喔哇喔好燃啊！！！",
+    "黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴黑琴",
     {
         content: "子弹是金属，枪也是金属，炮姐直接操控啊,子弹是金属",
         style: "color:yellow",
-        duration: 6000
-    }
+        duration: 8000,
+    },
 ];
 function getRandomIndex(len) {
     return ~~(Math.random() * len);
@@ -86,10 +88,14 @@ function startBatch() {
 }
 var isBigTest = false;
 var txtDanmuEl = document.getElementById("txtDanmu");
-document.getElementById("btnSend").addEventListener("click", function (ev) {
-    manager.send(txtDanmuEl.value);
-    ev.stopPropagation();
-}, false);
+document.getElementById("btnSend").addEventListener(
+    "click",
+    function (ev) {
+        manager.send(txtDanmuEl.value);
+        ev.stopPropagation();
+    },
+    false
+);
 document.getElementById("btnPause").addEventListener("click", function () {
     if (isBigTest) {
         clearInterval(ticket);
@@ -136,23 +142,34 @@ var lbTotal = document.getElementById("lbTotal");
 var lbHide = document.getElementById("lbHide");
 var lbInView = document.getElementById("lbInView");
 var rect = containerEl.getBoundingClientRect();
-var left = rect.left, width = rect.width;
+var left = rect.left,
+    width = rect.width;
 var right = left + width;
 setInterval(function () {
     window.requestIdleCallback(function () {
         var startTime = performance.now();
-        var allItems = Array.from(containerEl.querySelectorAll(".barrage-item"));
+        var allItems = Array.from(
+            containerEl.querySelectorAll(".barrage-item")
+        );
         var accCount = document.querySelectorAll(".barrage-item-acc").length;
         var len = allItems.length;
-        var inHideLen = allItems.filter(function (item) { return item.classList.contains("hide"); }).length;
+        var inHideLen = allItems.filter(function (item) {
+            return item.classList.contains("hide");
+        }).length;
         var inViewLen = allItems.filter(function (item) {
             var rect = item.getBoundingClientRect();
-            var b = !item.classList.contains("hide") && rect.left + rect.width >= left;
+            var b =
+                !item.classList.contains("hide") &&
+                rect.left + rect.width >= left;
             return b;
         }).length;
         lbTotal.innerText = len + "-" + accCount;
         lbHide.innerHTML = inHideLen + "";
-        lbInView.innerHTML = inViewLen + " (本次统计耗时" + (performance.now() - startTime).toFixed(2) + "ms)";
+        lbInView.innerHTML =
+            inViewLen +
+            " (本次统计耗时" +
+            (performance.now() - startTime).toFixed(2) +
+            "ms)";
     });
 }, 5000);
 document.addEventListener("visibilitychange", function () {
@@ -191,7 +208,7 @@ function goFull() {
     videoEl.setAttribute("webkit-playsinline", "");
     videoEl.setAttribute("playsinline", "");
     manager.resize({
-        duration: 10000
+        duration: 10000,
     });
 }
 function quitFull() {
@@ -200,6 +217,6 @@ function quitFull() {
     containerEl.classList.remove("fullScreen");
     videoEl.classList.remove("fullScreen");
     manager.resize({
-        duration: 8000
+        duration: 8000,
     });
 }
